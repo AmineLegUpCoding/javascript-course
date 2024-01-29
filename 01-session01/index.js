644b66415d25f91fceafc514f99322b2e8239d70
Amine legdani <legdani.amine@gmail.com>
2024-01-29 14:12:04 +0000

    test git

---
+++ 01-session01/index.js
@@ -0,0 +1,52 @@
+// Déclaration des variables
+/*  
+Les commentaires multilignes
+*/
+
+let nombreUn;
+let nombreDeux;
+let somme;
+
+const PI = 3.14;
+
+nombreUn = 16;
+nombreDeux = 20;
+
+somme = nombreUn + nombreDeux;
+
+
+
+// Les fonctions
+
+function greet(nom) {
+
+    console.log("Hellooo! " + nom)
+}
+
+greet("bakarouch")
+
+greet("boukri")
+
+greet("ait said")
+
+function sum (x, y){
+
+
+    return x + y
+}
+
+
+
+function inscrire(){
+    let nom = "AAAA";
+    let prenom = "SAMIH";
+    let age = 19;
+
+    if (age == 19) {
+        var statut = "etudiant"
+       
+    }
+    
+}
+console.log(statut)
+inscrire()
